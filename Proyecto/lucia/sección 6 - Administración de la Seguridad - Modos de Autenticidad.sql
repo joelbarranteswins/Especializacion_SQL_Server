346b75d2e2d7304df2eb6349b92680300f573fa8
luciapm25 <luciapimayuri@gmail.com>
2023-03-12 20:14:07 -0500

    Create sección 6 - Administración de la Seguridad - Modos de Autenticidad.sql
    autenticidad

---
+++ Proyecto/lucia/sección 6 - Administración de la Seguridad - Modos de Autenticidad.sql
@@ -0,0 +1,44 @@
+
+/*==============================================================*/
+/*==================ADMINISTRACION DE SEGURIDAD======================*/
+
+-- a. Modos de autenticidad
+
+
+
+-- b. Creación de Usuarios
+
+USE ECORP
+
+
+
+
+CREATE LOGIN ECORP_AREA_DE_TESORERIA WITH PASSWORD='12345'
+
+
+-- c. Uso de credenciales y permisos
+
+-- d. Roles del servidor
+
+SP_ADDSRVROLEMEMBER ECORP,[sysadmin]
+
+-- QUITAR RS SYSADMIN
+SP_DROPSRVROLEMEMBER ECORP, [sysadmin]
+GO
+-- AGREGAR RS DBCREATOR, PARA CREAR UNA BD
+SP_ADDSRVROLEMEMBER ECORP.[dbcreator]
+GO
+SP_DROPSRVROLEMEMBER ECORP,[dbcreator]
+GO
+
+-- AGREGAR RS SCURITYADMIN, CREAR UN LOGIN 
+SP_ADDSRVROLEMEMBER ECORP,[securityadmin]
+GO
+
+SP_DROPSRVROLEMEMBER ECORP, [securityadmin]
+
+
+-- e. Beneficios de la automatización
+
+
+
